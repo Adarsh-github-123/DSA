6e86dbc2d43e34bc96c64a0442f9fb1fd202c2b8
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-09-13 09:18:50 +0530

    Dutch National flag algo

---
+++ Array/sort012.cpp
@@ -0,0 +1,27 @@
+// Using Dutch National Flag Algorithm
+
+#include <bits/stdc++.h>
+void sort012(int *arr, int n)
+{
+    int low = 0, mid, high = n - 1;
+    mid = low;
+
+    while (mid <= high)
+    {
+        if (arr[mid] == 0)
+        {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
